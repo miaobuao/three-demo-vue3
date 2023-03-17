e445b53f38cb04df974495543410e48ea8db9995
喵不嗷 <617802189@qq.com>
2023-03-17 10:17:21 +0800

    init

---
+++ src/App.vue
@@ -0,0 +1,18 @@
+<template>
+  <div ref="container"></div>
+</template>
+
+<script setup lang="ts">
+import {onMounted, ref} from "vue"
+
+import * as THREE from 'three';
+
+const container = ref<HTMLElement|null>(null)
+
+const renderer = new THREE.WebGLRenderer({antialias: true})
+renderer.setSize(window.innerWidth, window.innerHeight)
+
+onMounted(()=>{
+  container.value?.append(renderer.domElement)
+})
+</script>
